9fde936be861a0a922ab20c9243be81c96e2478a
YashPatel0 <yashpatel22150@gmail.com>
2026-02-19 15:19:22 +0530

    update file

--- Ec2.tf
+++ Ec2.tf
@@ -6,19 +6,34 @@ resource "aws_instance" "my_ec2_instance" {
     ami = "ami-019715e0d74f695be"
     instance_type = "t3.small"
     key_name = var.key_name
-    vpc_security_group_ids = [var.sg_id,aws_security_group.my_sg.id]
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
     tags = {
         Name = "my-terr-ec2-instance"
         env = "dev"
     }
     # heredoc
     user_data = <<EOF
-    #!/bin/bash
-    apt update -y
-    apt install apache2 -y
-    systemctl enable apache2
-    systemctl start apache2
-    EOF
+#!/bin/bash
+set -e
+
+# Wait for network
+sleep 30
+
+# Update system
+apt-get update -y
+
+# Install Apache
+apt-get install -y apache2
+
+# Create simple test page
+echo "<h1>Deployed using Terraform</h1>" > /var/www/html/index.html
+
+# Enable and start Apache
+systemctl enable apache2
+systemctl start apache2
+
+EOF
+
 }
 
 resource "aws_security_group" "my_sg" {
@@ -59,10 +74,6 @@ data "aws_vpc" "my_vpc" {
 variable "key_name" {
     description = "Name of the AWS key pair to use for the EC2 instance"
     default = "aws_keypair"
-}
-
-variable "sg_id" {
-  default = "sg-0b680d925bb9efc3f"
 }
 
 output "public_ip" {
