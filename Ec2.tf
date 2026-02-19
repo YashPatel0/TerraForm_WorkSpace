314f2877df164e4c872ba592aca0cba87333d8e7
YashPatel0 <yashpatel22150@gmail.com>
2026-02-19 14:53:24 +0530

    commit

--- Ec2.tf
+++ Ec2.tf
@@ -6,7 +6,50 @@ resource "aws_instance" "my_ec2_instance" {
     ami = "ami-051a31ab2f4d498f5"
     instance_type = "t3.small"
     key_name = var.key_name
-    vpc_security_group_ids = [var.sg_id]
+    vpc_security_group_ids = [var.sg_id,aws_security_group.my_sg.id]
+    # heredoc
+    user_data = <<EOF
+    #!/bin/bash
+    apt update -y
+    apt install apache2 -y
+    systemctl enable apache2
+    systemctl start apache2
+    EOF
+}
+
+resource "aws_security_group" "my_sg" {
+    name = "new-sg"
+    description = "new security group"
+    vpc_id = data.aws_vpc.my_vpc.id
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+    }
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+    }
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+    }
+    egress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocol = -1        
+    }
+}
+
+data "aws_vpc" "my_vpc" {
+    default = true
 }
 
 variable "key_name" {
@@ -16,4 +59,8 @@ variable "key_name" {
 
 variable "sg_id" {
   default = "sg-0b680d925bb9efc3f"
+}
+
+output "public_ip" {
+    value = aws_instance.my_ec2_instance.public_ip
 }
