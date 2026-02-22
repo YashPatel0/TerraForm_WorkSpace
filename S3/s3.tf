9c292c13c4ef4808c2bccdaa252b1059e52c310c
YashPatel0 <yashpatel22150@gmail.com>
2026-02-22 14:45:35 +0530

    add lifecycle configuration for S3 bucket to manage object expiration

--- S3/s3.tf
+++ S3/s3.tf
@@ -22,4 +22,24 @@ resource "aws_s3_bucket_public_access_block" "yash_s3_bucket_public_access_block
     restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "yash_s3_bucket_lifecycle_configuration" {
+    bucket = aws_s3_bucket.yash_s3_bucket.bucket
+
+    rule {
+      id = "Expire old versions"
+      status = "Enabled"
+      transition {
+        days = 30
+        storage_class = "STANDARD_IA"
+      }
+      transition {
+        days = 60
+        storage_class = "GLACIER"
+      }
+      expiration {
+        days = 90
+      }
+    }
+}
+
 
