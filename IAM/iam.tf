4a40b0735169e48850cb1c71c68f56ee3b5fabe9
YashPatel0 <yashpatel22150@gmail.com>
2026-02-23 17:14:27 +0530

    feat: add IAM role configuration for EC2 service

---
+++ IAM/iam.tf
@@ -0,0 +1,21 @@
+provider "AWS" {
+  region = "ap-south-1"
+}
+
+resource "aws_iam_role" "demo_role" {
+    name = "demo_role"
+    assume_role_policy = jsondecode(
+        {
+            version = "2012-10-17"
+            statement = [
+                {
+                    effect = "Allow"
+                    principal = {
+                        service = "ec2.amazonaws.com"
+                    }
+                    action = "sts:AssumeRole"
+                }
+            ]
+        }
+    )
+}
