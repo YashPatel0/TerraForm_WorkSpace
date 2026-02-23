037f35e0111d0be77a78537e6bf10bc4c6413ad1
YashPatel0 <yashpatel22150@gmail.com>
2026-02-23 17:20:39 +0530

    fix: correct provider casing and format assume_role_policy in IAM role configuration

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -1,21 +1,20 @@
-provider "AWS" {
+provider "aws" {
   region = "ap-south-1"
 }
 
 resource "aws_iam_role" "demo_role" {
     name = "demo_role"
-    assume_role_policy = jsondecode(
-        {
-            version = "2012-10-17"
-            statement = [
-                {
-                    effect = "Allow"
-                    principal = {
-                        service = "ec2.amazonaws.com"
+    assume_role_policy = jsondecode({
+        version = "2012-10-17"
+        statement = [
+            {
+                effect = "Allow"
+                principal = {
+                    service = "ec2.amazonaws.com"
                     }
-                    action = "sts:AssumeRole"
-                }
-            ]
-        }
-    )
+                action = "sts:AssumeRole"
+            }
+        ]
+    })
 }
+
