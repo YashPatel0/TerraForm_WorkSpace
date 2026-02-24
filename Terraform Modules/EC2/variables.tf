531278eaeb23edba1c5bb3a49ce6db8bb5b3ff6a
YashPatel0 <yashpatel22150@gmail.com>
2026-02-24 17:41:37 +0530

    ec2 commit

---
+++ Terraform Modules/EC2/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+  default = "ami-019715e0d74f695be"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "key_name" {
+  default = "aws_keypair"
+}
+variable "tagname" {
+  default = "my_project"
+}
+variable "security_group_id" {
+
+}
+variable "subnet_id" {}
+
+variable "associate_public_ip" {}
+
